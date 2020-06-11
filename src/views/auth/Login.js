8f1b9f84db7885840b79b78ab0aad840e37911eb
iamsahebgiri <iamsahebgiri@gmail.com>
2020-06-11 17:34:09 +0530

    added the auth frontend ui

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,7 +1,102 @@
 import React from "react";
+import { Formik, Field, Form } from 'formik';
+import {
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Checkbox,
+  Link,
+  Text
+} from "@chakra-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import * as yup from 'yup';
 
 export default function Login() {
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email()
+      .required(),
+    password: yup
+      .string()
+      .required()
+      .min(3, 'Seems a bit short...')
+    // .matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$","Minimum eight characters, at least one letter and one number"),
+    ,
+    agreeToTerms: yup
+      .boolean()
+      .test(
+        'is-true',
+        'Must agree to terms to continue',
+        value => value === true
+      ),
+  });
   return (
-    <h1>Sign In</h1>
+    <>
+    <Text fontSize="4xl">Log in to your account</Text>
+      <Formik
+        validateOnChange={true}
+        initialValues={{ email: "", password: "", agreeToTerms: true }}
+        validationSchema={validationSchema}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          // make async call
+          setTimeout(() => {
+            console.log("submit: ", data);
+            setSubmitting(false);
+
+          }, 1000);
+        }}>
+
+        {({ values, errors, isSubmitting }) => (
+          <Form>
+            <Field name="email">
+              {({ field, form }) => {
+                return (
+                  <FormControl isInvalid={form.errors.email && form.touched.email} isRequired>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input {...field} id="email" placeholder="Email address" />
+                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                  </FormControl>
+                )
+              }}
+            </Field>
+            <Field name="password">
+              {({ field, form }) => {
+                return (
+                  <FormControl isInvalid={form.errors.password && form.touched.password} isRequired>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input {...field} type="password" id="password" placeholder="Enter password" />
+                    <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                  </FormControl>
+                )
+              }}
+            </Field>
+
+            <Button
+              mt={4}
+              variantColor="blue"
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              Login
+          </Button>
+            {/* <pre>{JSON.stringify(values, null, 2)}</pre>
+          <pre>{JSON.stringify(errors, null, 2)}</pre> */}
+          </Form>
+
+
+        )}
+      </Formik>
+      <Text>
+        Don't have an account?{" "}
+
+        <Link as={RouterLink} color="teal.500" to="/register">
+          Create account
+      </Link>
+      </Text>
+    </>
   );
 }
--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,55 +1,116 @@
 import React from "react";
-import { Formik, Field } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import {
   Button,
   Input,
   FormControl,
   FormLabel,
   FormErrorMessage,
+  Checkbox,
+  Link,
+  Text
 } from "@chakra-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import * as yup from 'yup';
 
 export default function Register() {
-
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email()
+      .required(),
+    password: yup
+      .string()
+      .required()
+      .min(3, 'Seems a bit short...')
+    // .matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$","Minimum eight characters, at least one letter and one number"),
+    ,
+    agreeToTerms: yup
+      .boolean()
+      .test(
+        'is-true',
+        'Must agree to terms to continue',
+        value => value === true
+      ),
+  });
   return (
-    <Formik
-      initialValues={{ name: "" }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-    >
-      {props => (
-        <form onSubmit={props.handleSubmit}>
-          <Field name="name" validate={validateName}>
-            {({ field, form }) => (
-              <FormControl isInvalid={form.errors.name && form.touched.name}>
-                <FormLabel htmlFor="name">Email address</FormLabel>
-                <Input {...field} id="name" placeholder="Enter email" />
-                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
-          <Field name="password" validate={validatePassword}>
-            {({ field, form }) => (
-              <FormControl isInvalid={form.errors.name && form.touched.name}>
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input {...field} id="name" placeholder="Enter password" />
-                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
-          <Button
-            mt={4}
-            variantColor="blue"
-            isLoading={props.isSubmitting}
-            type="submit"
-          >
-           Register
-              </Button>
-        </form>
-      )}
-    </Formik>
+    <>
+      <Text fontSize="4xl">Create account</Text>
+      <Formik
+        validateOnChange={true}
+        initialValues={{ email: "", password: "", agreeToTerms: true }}
+        validationSchema={validationSchema}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          // make async call
+          setTimeout(() => {
+            console.log("submit: ", data);
+            setSubmitting(false);
+
+          }, 1000);
+        }}>
+
+        {({ values, errors, isSubmitting }) => (
+          <Form>
+            <Field name="email">
+              {({ field, form }) => {
+                return (
+                  <FormControl isInvalid={form.errors.email && form.touched.email} isRequired>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input {...field} id="email" placeholder="Email address" />
+                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                  </FormControl>
+                )
+              }}
+            </Field>
+            <Field name="password">
+              {({ field, form }) => {
+                return (
+                  <FormControl isInvalid={form.errors.password && form.touched.password} isRequired>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input {...field} type="password" id="password" placeholder="Enter password" />
+                    <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                  </FormControl>
+                )
+              }}
+            </Field>
+            <Field>
+              {({ field, form }) => {
+                return (
+                  <FormControl isInvalid={form.errors.agreeToTerms && form.touched.agreeToTerms}>
+                    <Checkbox name="agreeToTerms" isChecked={field.value.agreeToTerms} onChange={form.handleChange} onBlur={form.handleBlur} defaultIsChecked isFullWidth isInvalid={!field.value.agreeToTerms}>I agree to the terms and coditions.</Checkbox>
+                    <FormErrorMessage>{form.errors.agreeToTerms}</FormErrorMessage>
+                  </FormControl>
+                )
+              }
+
+              }
+            </Field>
+
+            <br />
+
+            <Button
+              mt={4}
+              variantColor="blue"
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              Register
+          </Button>
+            {/* <pre>{JSON.stringify(values, null, 2)}</pre>
+          <pre>{JSON.stringify(errors, null, 2)}</pre> */}
+          </Form>
+
+
+        )}
+      </Formik>
+      <Text>
+        Already have an account?{" "}
+
+        <Link as={RouterLink} color="teal.500" to="/login">
+          Log in
+      </Link>
+      </Text>
+    </>
   );
 }
