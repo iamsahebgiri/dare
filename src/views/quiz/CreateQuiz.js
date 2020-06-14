fbe3c7893f24f4962b618ed1339c76ceb80b73d5
saheb Giri <sahebgiri578@gmail.com>
2020-06-14 10:23:24 +0530

    styled leaderboard

--- src/views/quiz/CreateQuiz.js
+++ src/views/quiz/CreateQuiz.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React from 'react';
+import DarkMode from '../../components/DarkMode';
+import { IconButton, Link, useColorMode } from "@chakra-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import { FiChevronLeft } from "react-icons/fi";
+import "./CreateQuiz.css";
 
 export default function CreateQuiz() {
+  const { colorMode } = useColorMode();
+  const color = { light: "gray.800", dark: "white" };
+  const optionColor = { light: { background: "white" }, dark: { background: "rgba(255,255,255,0.08" } };
   return (
     <div className="quiz-container">
-      <div className="quiz-questions-container">
-        <span>5/10</span>
-        <p>Isme sey class teacher ka kaun chatta tha?</p>
+      <div className="quiz-header">
+        <div className="back">
+          <Link as={RouterLink} to="/">
+            <IconButton isRound="true" size="sm" icon={FiChevronLeft} color={color[colorMode]} />
+          </Link>
+        </div>
+        <div className="quiz-title">1/20</div>
+        <div>
+          <DarkMode />
+        </div>
+      </div>
+      <div className="quiz-question-wrapper">
+        <div className="quiz-questions-container">
+          <p>Who among them is best in almost everything?</p>
+        </div>
+        <div className="fake-quiz-container"></div>
       </div>
       <div className="quiz-options">
-        <div className="direction">
+        <div className="direction" style={{ color: color[colorMode] }}>
           Choose the correct answer.
         </div>
-        <div className="option correct">
+        <div className="option correct" style={optionColor[colorMode]}>
           Amit
         </div>
-        <div className="option wrong">
+        <div className="option wrong" style={optionColor[colorMode]}>
           Ashish
         </div>
-        <div className="option">
+        <div className="option" style={optionColor[colorMode]}>
           Ankita
         </div>
-        <div className="option">
+        <div className="option" style={optionColor[colorMode]}>
           Swapna
         </div>
       </div>
--- src/views/user/Leaderboard.js
+++ src/views/user/Leaderboard.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, IconButton, useColorMode, Link, Stack, Text, Badge } from "@chakra-ui/core";
+import { FiChevronLeft } from "react-icons/fi";
+import { Link as RouterLink } from "react-router-dom";
+import "./Profile.css";
+import "./Leaderboard.css";
+import DarkMode from '../../components/DarkMode';
+
 
 export default function Leaderboard() {
+  const { colorMode } = useColorMode();
+  const color = { light: "gray.800", dark: "white" };
+  const optionColor = { light: { background: "white" }, dark: { background: "rgba(255,255,255,0.08" } };
   return (
-    <div>
-      
+    <div className="onboarding-container">
+      <div className="onboarding-heading">
+        <div className="back">
+          <Link as={RouterLink} to="/">
+            <IconButton isRound="true" size="sm" icon={FiChevronLeft}  color={color[colorMode]} />
+          </Link>
+        </div>
+        <div>Leaderboard</div>
+        <div>
+          <DarkMode />
+        </div>
+      </div>
+      <Stack isInline align="center" spacing={4}>
+        <div className="profile-picture">
+          <span role="img" aria-label="Emoji">😎</span>
+          <Avatar size="xl" name="Ryan Florence" src="https://bit.ly/ryan-florence" />
+          <Text isTruncated>Ryan</Text><Badge variantColor="green">10</Badge>
+        </div>
+        <div className="profile-picture">
+          <span role="img" aria-label="Emoji">👑</span>
+          <Avatar size="2xl" name="Christian Nwamba" src="https://bit.ly/code-beast" />
+          <Text isTruncated>Christian</Text><Badge variantColor="teal">30</Badge>
+        </div>
+        <div className="profile-picture">
+          <span role="img" aria-label="Emoji">😂</span>
+          <Avatar size="xl" name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+          <Text isTruncated>Kent</Text><Badge variantColor="blue">16</Badge>
+        </div>
+      </Stack>
+
+      <Stack spacing={4} mt={4} mb={10} pb={8}>
+        <div className="leaderboard-item active">
+          <div className="left-leaderboard-item">
+            <div className="num">4.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Riya (you)</div>
+          </div>
+          <div className="point">10</div>
+        </div>
+        <div className="leaderboard-item" style={optionColor[colorMode]}>
+          <div className="left-leaderboard-item">
+            <div className="num">5.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Ronnie</div>
+          </div>
+          <div className="point">9</div>
+        </div>
+        <div className="leaderboard-item" style={optionColor[colorMode]}>
+          <div className="left-leaderboard-item">
+            <div className="num">5.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Ronnie</div>
+          </div>
+          <div className="point">9</div>
+        </div>
+        <div className="leaderboard-item" style={optionColor[colorMode]}>
+          <div className="left-leaderboard-item">
+            <div className="num">5.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Ronnie</div>
+          </div>
+          <div className="point">9</div>
+        </div>
+        <div className="leaderboard-item" style={optionColor[colorMode]}>
+          <div className="left-leaderboard-item">
+            <div className="num">5.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Ronnie</div>
+          </div>
+          <div className="point">9</div>
+        </div>
+        <div className="leaderboard-item" style={optionColor[colorMode]}>
+          <div className="left-leaderboard-item">
+            <div className="num">5.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Ronnie</div>
+          </div>
+          <div className="point">9</div>
+        </div>
+        <div className="leaderboard-item" style={optionColor[colorMode]}>
+          <div className="left-leaderboard-item">
+            <div className="num">5.</div>
+            <Avatar size="sm" />
+            <div className="leaderboard-username">Ronnie</div>
+          </div>
+          <div className="point">9</div>
+        </div>
+      </Stack>
     </div>
   )
 }
