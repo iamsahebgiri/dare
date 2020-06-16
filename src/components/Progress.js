77c2f583e965e329fc97a30665bc8329da70f6b7
saheb Giri <sahebgiri578@gmail.com>
2020-06-16 18:57:27 +0530

    added nprogress

---
+++ src/components/Progress.js
@@ -0,0 +1,14 @@
+import { withNProgress } from '@tanem/react-nprogress'
+import React from 'react'
+import Bar from './Bar'
+import Container from './Container'
+import './index.css'
+// import Spinner from './Spinner'
+
+const Progress = ({ isFinished, progress, animationDuration }) => (
+  <Container isFinished={isFinished} animationDuration={animationDuration}>
+    <Bar progress={progress} animationDuration={animationDuration} />
+  </Container>
+)
+
+export default withNProgress(Progress)
